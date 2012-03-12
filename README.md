ff32f83ea823881eb4637a7004d1201066770830
Philipp Wagner <bytefish@gmx.de>
2012-03-13 00:18:25 +0100

    Documentation.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
+# Description
+
+[libfacerec](http://www.github.com/bytefish/libfacerec) is a header-only library for face recognition in OpenCV. Using it as simple as adding it to your include path, there are no additional dependencies needed. The Eigenfaces method and Fisherfaces method are implemented already, face recognition with Local Binary Patterns (LBP) is planned (almost completed). Please see [src/main.cpp](https://github.com/bytefish/libfacerec/blob/master/src/main.cpp) or the [tests](https://github.com/bytefish/libfacerec/tree/master/test) to get a feeling for the API. If you want to run the example, you'll need a CSV file with lines composed of a _filename_ followed by a _;_ followed by the _label_ (as **integer number**), making up a line like this: `/path/to/image.ext;0` ([read my notes on this here](http://www.bytefish.de/blog/fisherfaces_in_opencv)). 
+
+# Literature
+
+* Eigenfaces (Turk, M., and Pentland, A. "Eigenfaces for recognition.". Journal of Cognitive Neuroscience 3 (1991), 71–86.)
+* Fisherfaces (Belhumeur, P. N., Hespanha, J., and Kriegman, D. "Eigenfaces vs. Fisherfaces: Recognition using class specific linear projection.". IEEE Transactions on Pattern Analysis and Machine Intelligence 19, 7 (1997), 711–720.)
+* Local Binary Patterns Histograms (Ahonen, T., Hadid, A., and Pietikainen, M. "Face Recognition with Local Binary Patterns.". Computer Vision - ECCV 2004 (2004), 469–481.)
+
 # Warning
 
-This library is heavy **Work In Progress**. Once finished it can easily be used for face recognition with the OpenCV2 C++ API. The library will be a header-only library, so using it as simple as adding it to your include path. If you want to try this library, clone the repository and compile the src/main.cpp (you probably want to disable the tests in the CMakeLists.txt if you are using CMake).
-
-
-No bug reports in these early development stages please. Expect major interfaces changes coming.
+This library is still **Work in Progress**. No bug reports in these early development stages please. Expect major interface changes coming.
--- src/main.cpp
+++ src/main.cpp
@@ -19,16 +19,12 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
-#include "colormap.hpp"
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
-#include "subspace.hpp"
 #include "facerec.hpp"
 
-#include "opencv2/ts/ts.hpp"
 
 using namespace cv;
 using namespace std;
@@ -48,52 +44,6 @@ void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, 
 }
 
 int main(int argc, const char *argv[]) {
-    // Example for a Linear Discriminant Analysis
-    // (example taken from: http://www.bytefish.de/wiki/pca_lda_with_gnu_octave)
-    double d[11][2] = { { 2, 3 }, { 3, 4 }, { 4, 5 }, { 5, 6 }, { 5, 7 }, { 2,
-            1 }, { 3, 2 }, { 4, 2 }, { 4, 3 }, { 6, 4 }, { 7, 6 } };
-    int c[11] = { 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1 };
-    // convert into OpenCV representation
-    Mat _data = Mat(11, 2, CV_64FC1, d).clone();
-    vector<int> _classes(c, c + sizeof(c) / sizeof(int));
-    // perform the lda
-    subspace::LDA lda(_data, _classes);
-    // GNU Octave finds the following Eigenvalue:
-    //octave> d
-    //d =
-    //	 1.5195e+00
-    //
-    // Eigen finds the following Eigenvalue:
-    // [1.519536390756363]
-    //
-    // Since there's only 1 discriminant, this is correct.
-    cout << "Eigenvalues:" << endl << lda.eigenvalues() << endl;
-    // GNU Octave finds the following Eigenvectors:
-    //	octave:13> V(:,1)
-    //	V =
-    //
-    //	   0.71169  -0.96623
-    //	  -0.70249  -0.25766
-    //
-    // Eigen finds the following Eigenvector:
-    // [0.7116932742510111;
-    //  -0.702490343980524 ]
-    //
-    cout << "Eigenvectors:" << endl << lda.eigenvectors() << endl;
-    // project a data sample onto the subspace identified by LDA
-    Mat x = _data.row(0);
-    cout << "Projection of " << x << ": " << endl;
-    cout << lda.project(x) << endl;
-
-    cout << lda.reconstruct(lda.project(x)) << endl;
-    // example for reading a face database from a CSV file
-    //
-    // CSV -- https://github.com/bytefish/opencv/blob/master/lda/at.txt
-    // Database -- http://www.cl.cam.ac.uk/research/dtg/attarchive/facedatabase.html
-    //
-    // Always make sure classes are given as {0, 1,..., n}!
-    vector<Mat> images;
-    vector<int> labels;
     // check for command line arguments
     if (argc != 2) {
         cout << "usage: " << argv[0] << " <csv.ext>" << endl;
@@ -101,7 +51,10 @@ int main(int argc, const char *argv[]) {
     }
     // path to your CSV
     string fn_csv = string(argv[1]);
-    // read in the images
+    // images and corresponding labels
+    vector<Mat> images;
+    vector<int> labels;
+    // read in the data
     try {
         read_csv(fn_csv, images, labels);
     } catch (exception& e) {
