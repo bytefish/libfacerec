6dd91a18063dd468d87afafeddf82f0305ee547f
Philipp Wagner <bytefish@gmx.de>
2012-06-03 23:28:07 +0200

    Add subspace tests stub.

---
+++ test/test_subspace.cpp
@@ -0,0 +1,28 @@
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/ts/ts.hpp"
+
+// some helper methods for testing
+#include "test_funs.hpp"
+#include "helper.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+//------------------------------------------------------------------------------
+// subspace::project
+//------------------------------------------------------------------------------
+TEST(TestSubspace, subspaceProject) {
+    // TODO
+    ASSERT_TRUE(false);
+}
+
+//------------------------------------------------------------------------------
+// subspace::reconstruct
+//------------------------------------------------------------------------------
+TEST(TestSubspace, subspaceReconstruct) {
+    // TODO
+    ASSERT_TRUE(false);
+}
+
