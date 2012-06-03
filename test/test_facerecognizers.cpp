adc21b312f2c2a76529fa97ade64557773611401
Philipp Wagner <bytefish@gmx.de>
2012-06-03 23:47:05 +0200

    Added test_facerecognizers stub, so I don't forget to implement them.

---
+++ test/test_facerecognizers.cpp
@@ -0,0 +1,37 @@
+
+#include "opencv2/opencv.hpp"
+#include "opencv2/ts/ts.hpp"
+
+// some helper methods for testing
+#include "test_funs.hpp"
+#include "helper.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+// What to use: Image database? Synthetic datasets?
+
+//------------------------------------------------------------------------------
+// cv::Eigenfaces
+//------------------------------------------------------------------------------
+TEST(TestEigenfaces, train) {
+    // TODO
+    ASSERT_TRUE(false);
+}
+
+//------------------------------------------------------------------------------
+// cv::Fisherfaces
+//------------------------------------------------------------------------------
+TEST(TestFisherfaces, train) {
+    // TODO
+    ASSERT_TRUE(false);
+}
+
+//------------------------------------------------------------------------------
+// cv::LBPH
+//------------------------------------------------------------------------------
+TEST(TestLBPH, train) {
+    // TODO
+    ASSERT_TRUE(false);
+}
