9401b17bbf9fc49fe616dcb782edc58852ed623d
Philipp Wagner <bytefish@gmx.de>
2012-06-26 01:29:24 +0200

    Include corrected.

--- doc/source/src/facerec_save_load.cpp
+++ doc/source/src/facerec_save_load.cpp
@@ -16,10 +16,9 @@
  *   See <http://www.opensource.org/licenses/bsd-license>
  */
 
+#include "opencv2/contrib/contrib.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
-
-#include "facerec.hpp"
 
 #include <iostream>
 #include <fstream>
